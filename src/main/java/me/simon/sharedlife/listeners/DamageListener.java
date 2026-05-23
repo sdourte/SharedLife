88503a99746a85b69e1046913a062e0b5b5d86bc
Dourte Simon <simon.dourte@student.unamur.be>
2026-05-23 19:25:01 +0200

    Ajout d'une mention pour savoir qui perd de la vie

--- src/main/java/me/simon/sharedlife/listeners/DamageListener.java
+++ src/main/java/me/simon/sharedlife/listeners/DamageListener.java
@@ -2,6 +2,11 @@ package me.simon.sharedlife.listeners;
 
 import me.simon.sharedlife.SharedLife;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import org.bukkit.Bukkit;
+
 import org.bukkit.entity.Player;
 
 import org.bukkit.event.EventHandler;
@@ -34,6 +39,55 @@ public class DamageListener
                 instanceof Player player)) {
 
             return;
+        }
+
+        /*
+         * Dégâts réels.
+         */
+        double damage =
+                event.getFinalDamage();
+
+        /*
+         * Ignore dégâts nuls.
+         */
+        if (damage <= 0) {
+            return;
+        }
+
+        /*
+         * Conversion en coeurs.
+         */
+        double hearts =
+                damage / 2.0;
+
+        /*
+         * Arrondi 1 décimale.
+         */
+        hearts =
+                Math.round(
+                        hearts * 10.0
+                ) / 10.0;
+
+        /*
+         * Message action bar.
+         */
+        String message =
+                "§c"
+                        + player.getName()
+                        + " a perdu §4"
+                        + hearts
+                        + " ❤";
+
+        /*
+         * Envoie à tous.
+         */
+        for (Player onlinePlayer
+                : Bukkit.getOnlinePlayers()) {
+
+            onlinePlayer.spigot().sendMessage(
+                    ChatMessageType.ACTION_BAR,
+                    new TextComponent(message)
+            );
         }
 
         /*
